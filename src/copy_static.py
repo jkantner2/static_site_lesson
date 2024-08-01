4ce2d6a8d55128d7bb91e00af2899bdd89436c7d
jkantner2 <kantnerj2@gmail.com>
2024-07-31 22:50:18 -0400

    refactor copy_static

--- src/copy_static.py
+++ src/copy_static.py
@@ -2,36 +2,16 @@ import os
 import shutil
 
 
-def copy_static(path):
-    if os.path.exists("/Users/james/bootdevLessons/static_site_lesson/public"):
-        shutil.rmtree("/Users/james/bootdevLessons/static_site_lesson/public")
+def copy_files_directories(source_path, dest_path):
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
 
-    if not os.path.exists(path):
-        raise OSError("No such file or directory")
-    
-    if not os.path.exists("/Users/james/bootdevLessons/static_site_lesson/public"):
-        os.mkdir("/Users/james/bootdevLessons/static_site_lesson/public")
-    
-    copy_files_directories(path)
-
-
-def copy_files_directories(path):
-    dir_contents = os.listdir(path)
-    public_base_path = "/Users/james/bootdevLessons/static_site_lesson/public"
-    path_list = []
-    for item in dir_contents:
-        static_path = os.path.join(path, item)
-        tmp = static_path.split("static_site_lesson/static")
-        public_path = public_base_path + tmp[1]
-        if os.path.isdir(static_path):
-            if not os.path.exists(public_path):
-                os.mkdir(public_path)
-            path_list.append(public_path)
-            copy_files_directories(static_path)
-        if os.path.isfile(static_path):
-            path_list.append(static_path)
-            shutil.copy(static_path, public_path)
+    for item in os.listdir(source_path):
+        from_path = os.path.join(source_path, item)
+        to_path = os.path.join(dest_path, item)
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_files_directories(from_path, to_path)
 
 
-if __name__ == '__main__':
-    copy_static()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-from copy_static import copy_static
+import os
+import shutil
+
+from copy_static import copy_files_directories
 
 
 def main():
 
-    copy_static("/Users/james/bootdevLessons/static_site_lesson/static")
+    source_path = "./static/"
+    dest_path = "./public/"
+
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+
+    copy_files_directories(source_path, dest_path)
 
 
 if __name__ == "__main__":
